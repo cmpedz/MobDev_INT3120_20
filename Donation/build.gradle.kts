7553a581e0977d038087f2a3466e6f35679f9ca0
Your Name <you@example.com>
2023-09-09 07:40:33 +0700

    belongs to lesson 2

---
+++ Donation/build.gradle.kts
@@ -0,0 +1,11 @@
+buildscript {
+    val agp_version by extra("8.0.0")
+    val agp_version1 by extra("8.1.1")
+    val agp_version2 by extra("8.1.1")
+}
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.1.1" apply false
+//    id("com.android.library") version "8.1.0" apply false
+//    id("org.jetbrains.kotlin.android") version "1.5.31" apply false
+}
